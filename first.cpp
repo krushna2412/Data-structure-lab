2fe4678858bb12ef4e5f3cc79d25e159889fab47
krushna2412 <krushnaraut2412@gmail.com>
2026-08-26 12:56:25 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,42 @@
+ #include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int id1,id2,id3;
+    string title1,title2,title3;
+
+    cout << "Enter Book id1: ";
+    cin >> id1;
+
+    cin.ignore();
+    cout << "Enter Book Title1: ";
+    getline(cin, title1);
+
+    cout << "Enter Book id2: ";
+    cin >> id2;
+
+    cin.ignore();
+    cout << "Enter Book Title2: ";
+    getline(cin, title2);
+
+    cout << "Enter Book id3: ";
+    cin >> id3;
+
+    cin.ignore();
+    cout << "Enter Book Title3: ";
+    getline(cin, title3);
+
+    //dislay
+
+    cout<< "\n\n";
+    cout<< "\n Book Id1 = " << id1;
+    cout<< "\t\t\t Book Title1 = "<< title1;
+    cout<< "\n Book Id2 = " << id2;
+    cout<< "\t\t\t Book Title2 = " << title2;
+    cout<< "\n Book Id3 = " << id3;
+    cout<< "\t\t\t Book Title3 = " << title3;
+    cout<< "\n\n";
+
+    return 0;
+}
