e8889edf0c88d2ba98828be5cf0dac125547aa70
Krushna_Raut <krushnaraut2412@gmail.com>
2026-09-09 11:41:54 +0530

    Add book sorting functionality in library_sort.cpp
    Implement a simple sorting algorithm for book IDs.

---
+++ library_sort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int book[5];         //Declaration of array
+int searchID;
+
+cout<<"Enter 5 book IDs:\n";
+for(int i=0;i<5;i++)   //Assigning values
+   {
+cin>> book[i];
+   }
+// Sorting
+for(int i = 0; i< 4; i++)
+{
+  for (int j = 0; j < 4 - i; j++)
+{
+  if (book[j] > book [j + 1])
+{
+  int temp = book[j];
+  book[j] = book[j + 1];
+  book[j + 1] = temp;
+}
+}
+}
+cout << "\nBooks after sorting:\n";
+for (int i = 0; i < 5; i++)
+{
+  cout << book[i] << " ";
+}
+
+return 0;
+}
